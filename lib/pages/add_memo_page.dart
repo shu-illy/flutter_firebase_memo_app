18923c9770229efc5e85634ecf5747c4199f9d32
Shuhei Iriyama <67177058+shu-illy@users.noreply.github.com>
2021-09-15 13:31:15 +0900

    ページタイトルを中央寄せ

--- lib/pages/add_memo_page.dart
+++ lib/pages/add_memo_page.dart
@@ -14,6 +14,7 @@ class _AddMemoPageState extends State<AddMemoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('メモを追加'),
       ),
       body: Center(
